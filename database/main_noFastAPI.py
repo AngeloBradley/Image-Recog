2ad816bac91d28650afcedb932c589076b3e003e
Angelo Bradley <angelobradley919@gmail.com>
2020-11-18 17:59:23 -0500

    fixed bug in previous commit

--- database/main_noFastAPI.py
+++ database/main_noFastAPI.py
@@ -83,36 +83,37 @@ def post(data: Data):
         # Update official_captions file and caption pool
         if caption in official_captions_set:
             # read caption.json file line by line into a list, split each line into list by ' '
-            caption_dot_json = open(caption_pool_location + caption + '.txt', 'r')
-            lines = caption_dot_json.readlines()
-            caption_dot_json.close()
+            caption_dot_text = open(caption_pool_location + caption + '.txt', 'r')
+            lines = caption_dot_text.readlines()
+            caption_dot_text.close()
 
-            caption_dot_json_data = []
+            caption_dot_text_data = []
             for line in lines:
                 l = line.split(' ')
-                caption_dot_json_data.append(l)
+                l[1] = float(l[1])
+                caption_dot_text_data.append(l)
             
             # add new data to list
-            caption_dot_json_data.append([data.uuid, confidence])
+            caption_dot_text_data.append([data.uuid, confidence])
 
             # sort using confidence
-            caption_dot_json_data = [[x, str(y)] for x,y in sorted(caption_dot_json_data, key= lambda x:x[1])]
+            caption_dot_text_data = [[x, str(y)] for x,y in sorted(caption_dot_text_data, key= lambda x:x[1])]
 
             # print back to file line by line ' '.join(caption_data) -> "image_name confidence\n"
-            caption_dot_json = open(caption_pool_location + caption + '.txt', 'w')
-            caption_dot_json.write('')
-            caption_dot_json.close()
+            caption_dot_text = open(caption_pool_location + caption + '.txt', 'w')
+            caption_dot_text.write('')
+            caption_dot_text.close()
 
-            caption_dot_json = open(caption_pool_location + caption + '.txt', 'a')
-            for item in caption_dot_json_data:
+            caption_dot_text = open(caption_pool_location + caption + '.txt', 'a')
+            for item in caption_dot_text_data:
                 print(item)
-                caption_dot_json.write(' '.join(item))
+                caption_dot_text.write(' '.join(item) + '\n')
             # close file
-            caption_dot_json.close()
+            caption_dot_text.close()
             pass
         else:
             # create new caption.json file
-            
+
             # write caption info in the form -> "image_name confidence\n"
             # close file
             pass
