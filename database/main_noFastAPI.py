50bd6216138933989660b554b06b5a481bbefc99
Angelo Bradley <angelobradley919@gmail.com>
2020-11-18 16:29:13 -0500

    caption pool and official captions files updating

--- database/main_noFastAPI.py
+++ database/main_noFastAPI.py
@@ -1,4 +1,5 @@
 from dataclasses import dataclass, asdict
+from os import error
 from PyDictionary import PyDictionary
 import cv2 as cv
 import numpy as np
@@ -12,6 +13,7 @@ dictionary_file = 'cache/dictionary.json'
 official_captions_file = 'cache/official_captions.txt'
 official_captions_set = set()
 dictionary = {}
+
 
 @dataclass
 class Data:
@@ -29,14 +31,13 @@ class Data:
     #     self.captions = data['captions']
 
 
-
 def post(data: Data):
     # data_dict = data.dict()
 
     # add image file to cache
     image = np.asarray(data.image)
     cv.imwrite(cache_location + data.uuid + ".jpg", image)
-    
+
     # add metadata file to cache
     data_file = open(cache_location + data.uuid + ".json", "w")
     data_file.write(json.dumps(asdict(data)))
@@ -49,9 +50,10 @@ def post(data: Data):
     # caption_data format -> [caption, confidence]
 
     for caption_data in caption_list:
-        
+
         # Update the dictionary
         caption = caption_data[0]
+        confidence = caption_data[1]
         try:
             '''
                 Try to map the caption to itself. If the caption is not in 
@@ -61,7 +63,7 @@ def post(data: Data):
                 captions.
             '''
             dictionary[caption].add(caption)
-            
+
         except KeyError:
             # add caption to dictionary and map to set containing itself
             dictionary[caption] = set(caption)
@@ -78,27 +80,38 @@ def post(data: Data):
                         # map the synonym to a set containing the caption
                         dictionary[synonym] = set(caption)
 
-        # Update official_captions file
+        # Update official_captions file and caption pool
         if caption in official_captions_set:
-            continue
+            # Add image name to caption.json file
+            with open(caption_pool_location + caption + '.json', 'a+') as caption_dot_json:
+                # file_data = json.loads(caption_dot_json)
+                pass
+                # file_data[data.uuid] = confidence;
+                # file_data = sorted(file_data, lambda x:x[1])
         else:
+            # Add caption to official_caption_set and update file
             official_captions_set.add(caption)
 
+            # Overwrite official_captions_file with current official_captions_set state
             ocf = open(official_captions_file, 'w')
-            data = ocf.write(str('\n'.join(sorted(official_captions_set))))
+            ocf.write(str('\n'.join(sorted(official_captions_set))))
             ocf.close()
+
+            # Create caption.json file and add file name
+            with open(caption_pool_location + caption + '.json', 'w') as caption_dot_json:
+                caption_dot_json.write({json.dumps([data.uuid, confidence])})
+
 
 if __name__ == "__main__":
     image = cv.imread('../Image Repository/image.jpg')
     with open('cache/5ad38c9f-1dcf-47b8-b21b-97c171205cac.json') as file:
         data = json.load(file)
-        data['captions'] = [['fire hydrant', .345234],['bird', .2586234],['giraffe', .23532424]]
-        data = Data(data['original_name'], data['uuid'], data['image'], data['image_shape'], data['captions'])
+        data['captions'] = [['fire hydrant', .345234], [
+            'bird', .2586234], ['giraffe', .23532424], ['dog', .2848659]]
+        data = Data(data['original_name'], data['uuid'],
+                    data['image'], data['image_shape'], data['captions'])
         post(data)
 
-
-
-    
     # cv.imshow('image', image)
     # cv.waitKey(0)
     # cv.destroyAllWindows()
