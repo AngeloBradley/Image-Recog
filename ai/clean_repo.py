92d36d7d806945f8e1a881d3f7a1c0d799041b19
Angelo Bradley <angelobradley919@gmail.com>
2020-11-20 06:46:15 -0500

    updated requirements.txt for all modules

--- ai/clean_repo.py
+++
@@ -1,9 +0,0 @@
-import os
-
-image_repo = '../../Image Repository/'
-IMAGE_TYPES = ('.jpg', '.jpeg', '.tiff', '.png')
-
-for root, dirs, files in os.walk(image_repo):
-    for file in files:
-        if file[file.rfind('.'):] not in IMAGE_TYPES:
-            os.remove(image_repo + file)
